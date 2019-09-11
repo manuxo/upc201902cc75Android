69c340aee3eb84db972783452b9044f0b1bc4384
jmayta1984 <Monica$2017>
2019-09-11 00:05:27 -0500

    Ejercicio de consumo de servicios REST

---
+++ Pokeapi/app/src/main/java/pe/edu/upc/pokeapi/MainActivity.kt
@@ -0,0 +1,62 @@
+package pe.edu.upc.pokeapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: PokemonAdapter
+    var pokemons = ArrayList<Pokemon>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getpokemons()
+
+
+        rvPokemon.layoutManager = LinearLayoutManager(this)
+
+    }
+
+    private fun getpokemons() {
+
+        var retrofit = Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        var pokemonInterface = retrofit.create(PokemonInterface::class.java)
+
+        var methodPokemonsData: Call<PokedexResponse> = pokemonInterface.getPokemonsData()
+
+        methodPokemonsData.enqueue(object : Callback<PokedexResponse> {
+            override fun onFailure(call: Call<PokedexResponse>, t: Throwable) {
+                Log.d("Excepción: ", t.toString())
+            }
+
+            override fun onResponse(
+                call: Call<PokedexResponse>,
+                response: Response<PokedexResponse>
+            ) {
+
+                if (response.isSuccessful) {
+                    pokemons = response!!.body()!!.results
+                    adapter = PokemonAdapter(pokemons)
+                    rvPokemon.adapter = adapter
+                }
+
+            }
+
+        })
+
+    }
+}
